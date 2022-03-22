74c1d9d769c32f9fcca21916b706c1d44eec314d
AnnyXi <xiziyuan888@gmail.com>
2022-03-22 11:04:06 +0100

    js update
    none shoping cart showing up. .. strange

--- pages/webshop.php
+++ pages/webshop.php
@@ -9,7 +9,12 @@ if (!isset($_SESSION["ID"])&&$_SESSION["STATUS"] !="ACTIEF") {
 
 ?>
 <!--This is a search engine-->
-<script src="js/webshop.js"></script>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <script src="js/webshop.js"></script>
+</head>
+<body>
 <div class="content">
     <form name="search" id="search" action="" method="POST">
         <div style="background-color: #C2F98E; height: 25px; margin-top: 5%; padding:0;">
@@ -18,9 +23,9 @@ if (!isset($_SESSION["ID"])&&$_SESSION["STATUS"] !="ACTIEF") {
         </div>
     </form>
 </div>
-<?php
 
-//Here will the action be create
+<?php
+//Here will the search engine action be create
 if (isset($_POST["zoeken"])&& !empty($_POST["patroon"])) {
     $patroon = htmlspecialchars($_POST["patroon"]);
     $sql = "SELECT * FROM album WHERE titel LIKE '%$patroon%' || artiest LIKE '%$patroon%' || genre LIKE '%$patroon%'";
@@ -29,6 +34,7 @@ if (isset($_POST["zoeken"])&& !empty($_POST["patroon"])) {
 }
 ?>
 
+<!-- Menu list will be creat-->
 <div class="content">
     <form name="webshop" id="webshop" action="index.php?page=bestellen" method="POST">
         <?php
@@ -37,7 +43,7 @@ if (isset($_POST["zoeken"])&& !empty($_POST["patroon"])) {
         $albums = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $lus = 0;
         foreach($albums as $album){
-            echo "<img width '100px' src='img/" . $album['cover']. "'/>";
+            echo "<img width='100px' src='img/" . $album['cover']. "'/>";
             echo "<input type='hidden' name= 'id[$lus]' id= 'id[$lus]' value= '" . $album['ID'] . "'/>";
             echo "<input type='hidden' name= 'titel[$lus]' id= 'titel[$lus]' value= '" . $album['titel'] . "'/>";
             echo "<input type='hidden' name= 'artiest[$lus]' id= 'artiest[$lus]' value= '" . $album['artiest'] . "'/>";
@@ -46,10 +52,28 @@ if (isset($_POST["zoeken"])&& !empty($_POST["patroon"])) {
             echo "<br> Titel: " . $album["titel"] . " prijs: " . $album["prijs"];
             echo "<br> Vooraad: " . $album["voorraad"];
             echo "<br> Aantal: ";
-            echo "<input class='aantal' type='text' style='width 10%;' name='aantal[$lus]' id='aantal[$lus]' value='0' />";
+            echo "<input class='aantal' type='text' name='aantal[$lus]' id='aantal[$lus]' value='0' />";
+            echo "<hr />";
             $lus++;
         }
         echo "<input type='hidden' name='lus' id='lus' value='".$lus."'/>";
         ?>
+<!-- Winkelmandje aan- en uitzetten -->
+        <input type="checkbox" name="winkelmandje_knop" id="winkelmandje_knop" onclick="javascript:showWinkelmandje();" class="icon" value="&#128717;" />
+        <label for="winkelmandje_knop">&#128717;</label>
+        <br>
+        <div class="icon_container">
+            <input type="submit" class="icon" id="submit" name="submit" value="&rarr;" />
+        </div>
+        <br>
     </form>
 </div>
+<!-- Winkelmandje -->
+<div id="winkelmandje">
+    <div id="rijen"></div>
+    <input type="checkbox" id="winkelmandje_knop" onclick="javascript:showWinkelmandje();" class="icon" value="X" />
+    <label for="winkelmandje_knop" style="color:red;" >&#8861;</label>
+    
+</div>
+</body>
+</html>
